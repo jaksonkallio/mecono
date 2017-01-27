c4d08eaefea621ac73ac0149a3d9387b6a3748fe
Jakson Kallio <jakson@protonmail.com>
2017-01-27 10:09:48 -0600

    Self Node basics

--- src/SelfNode.h
+++ src/SelfNode.h
@@ -2,7 +2,22 @@
 @description The object representing the self-node
 @author Jakson R. Kallio
 */
+
 #ifndef SELFNODE_
 #define SELFNODE_
+
+#include <vector>
+
+class SelfNode {
+private:
+	std::string privateKey;
+	int known_path_count;
+
+	// A full path between this node
+	// std::vector<Path> paths_to;
+public:
+	std::string getPublicKey() const;
+	int getKnownPathCount() const;
+};
 
 #endif
