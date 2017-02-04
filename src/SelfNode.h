24db7aecabe7c362935640b9896311d4242ae772
jaksonkallio <jakson@protonmail.com>
2017-02-03 21:43:05 -0600

    SelfNode configuration constants

--- src/SelfNode.h
+++ src/SelfNode.h
@@ -10,12 +10,25 @@
 
 class SelfNode {
 private:
-	std::string privateKey;
-	int known_path_count;
-	
+  // Private key credentials	
+  std::string privateKey;
+  
+  // -- Configuration Variables -- See https://github.com/jaksonkallio/mecono/blob/master/documentation.md#configuration
+  const int community_hop_radius;
+  const std::string node_label;
+  const int hop_forward_limit_ping;
+  const int hop_forward_limit_chunk;
+  const int max_inbound_queue_time;
+  // ^^ END ^^
 public:
+  // The public key credential
 	std::string getPublicKey() const;
-	int getKnownPathCount() const;
+  
+  // Count of known path segments
+  unsigned int getKnownPathCount() const;
+
+  // Count of all known remote nodes
+  unsigned int getRemoteNodesCount() const;
 };
 
 #endif
