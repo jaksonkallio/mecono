25d5d5af2f91a6868f14a54a2c337b0e87c8168b
Jakson Kallio <jakson@protonmail.com>
2017-02-09 11:58:20 -0600

    SimNodes

--- src/RemoteNode.h
+++ src/RemoteNode.h
@@ -8,13 +8,16 @@
 
 #include <vector>
 
-class SelfNode {
+class RemoteNode {
 private:
 	std::string privateKey;
 	int known_path_count;
 
 	// Valid full paths to this node
 	std::vector<Path> paths_to;
+
+	// An array of neighbors this node is connected to
+	std::vector<RemoteNode> neighbors;
 public:
 	std::string getPublicKey() const;
 	int getKnownPathCount() const;
---
+++ src/SimNode.h
@@ -0,0 +1,19 @@
+/*
+@description A remote node
+@author Jakson R. Kallio
+*/
+
+#ifndef SIMNODE_
+#define SIMNODE_
+
+class SimNode: public RemoteNode {
+private:
+	// If the node will only process ping chunks, but no other kind of chunks
+	bool only_process_pings = false;
+
+	// Chance of node to flat out silently ignore chunks
+	double chance_ignore = 0.00;
+
+	// Simulated latency in milliseconds
+	unsigned int latency = 0;
+};
