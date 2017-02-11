0657edd7e54abd0fad8e27451b6438ab94ee1326
Jakson Kallio <jakson@protonmail.com>
2017-02-10 20:01:21 -0600

    SimNode neighbor functions

---
+++ src/SimNode.cpp
@@ -0,0 +1,9 @@
+unsigned short int SimNode::neighborCount() const{
+	return neighbors.size();
+}
+
+bool SimNode::hasNeighbor(std::string neighbor_address) const{
+	for(unsigned int i = 0; i < neighborCount(); ++i){
+		if
+	}
+}
