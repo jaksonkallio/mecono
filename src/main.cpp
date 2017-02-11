530cc6cf0743704506eb5039f98af2ca4235d4b4
Jakson Kallio <jakson@protonmail.com>
2017-02-10 21:36:58 -0600

    Testing list nodes

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,15 @@
 #include "SimNode.h"
 
 int main(){
-	// SimNetwork* sim = new SimNetwork();
-	// sim->drawNetworkGrid();
+	SimNetwork* sim = new SimNetwork();
+	//sim->drawNetworkGrid();
+	sim->listNodes();
 
+	/*
 	SimNode* testnode = new SimNode();
 	testnode->genAddress();
 	std::cout << testnode->getAddress() << "\n";
-	
+	*/
+
 	return EXIT_SUCCESS;
 }
