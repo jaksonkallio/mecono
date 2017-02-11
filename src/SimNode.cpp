32d9bcd2e885000b3f2a501171bbd520091afb3b
Jakson Kallio <jakson@protonmail.com>
2017-02-10 20:07:19 -0600

    Has neighbor

--- src/SimNode.cpp
+++ src/SimNode.cpp
@@ -2,8 +2,14 @@ unsigned short int SimNode::neighborCount() const{
 	return neighbors.size();
 }
 
-bool SimNode::hasNeighbor(std::string neighbor_address) const{
-	for(unsigned int i = 0; i < neighborCount(); ++i){
-		if
+bool SimNode::hasNeighbor(SimNode* neighbor) const{
+	bool neighborship(false);
+
+	for(unsigned short int i = 0; i < neighborCount(); ++i){
+		if(neighbors[i] == neighbor->getAddress()){
+			neighborship = true;
+		}
 	}
+
+	return neighborship;
 }
