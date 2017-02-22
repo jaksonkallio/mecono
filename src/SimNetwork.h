ece61ddea4095a007117d0da98bc1f8ef7f83309
Jakson Kallio <jakson@protonmail.com>
2017-02-22 10:36:50 -0600

    Simulation configuration section

--- src/SimNetwork.h
+++ src/SimNetwork.h
@@ -20,6 +20,7 @@ private:
 	const unsigned int columns = 5;
 	const unsigned int node_count = rows * columns;
 
+	// Simulation Configuration
 	static const unsigned short int neighbor_connectivity = 50;
 
 	unsigned int neighbor_connections = 0;
