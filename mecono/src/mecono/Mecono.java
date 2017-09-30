cef134dbc3098b839024ada96c9eb14046d3af87
Jakson Kallio <jakson@protonmail.com>
2017-09-30 12:33:50 -0500

    Start the simnet if --sim is set

--- mecono/src/mecono/Mecono.java
+++ mecono/src/mecono/Mecono.java
@@ -10,6 +10,11 @@ public class Mecono {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        for (String s: args) {
+            if (s.equals("--sim")) {
+                SimNetwork sim = new SimNetwork();
+                break;
+            }
+        }
     }
 }
