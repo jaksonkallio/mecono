b1e3076bf68de9eaeeb750b38ec8404d3c2fc2c2
Jakson Kallio <jakson@protonmail.com>
2017-10-01 13:47:50 -0500

    RemoteNode variables and getters

--- mecono/src/mecono/RemoteNode.java
+++ mecono/src/mecono/RemoteNode.java
@@ -1,5 +1,7 @@
 package mecono;
 
+import java.util.Set;
+
 /**
  *
  * @author jak
@@ -18,6 +20,40 @@ public class RemoteNode implements Node {
 		return label;
 	}
 	
-    protected String address;
-	protected String label;
+	public int countSuccessfulChunks() {
+		return successful_chunks;
+	}
+	
+	public int countFailedChunks() {
+		return failed_chunks;
+	}
+	
+	public int countReceivedChunks() {
+		return received_chunks;
+	}
+	
+	public boolean isCooperative() {
+		return cooperative;
+	}
+	
+	public boolean isBlacklisted() {
+		return blacklisted;
+	}
+	
+	public Set<RemoteNode> getNeighbors() {
+		return neighbors;
+	}
+	
+	public int getNeighborCount() {
+		return neighbors.size();
+	}
+	
+    private String address;
+	private String label;
+	private int successful_chunks; // Number of successful chunks sent to this node.
+	private int failed_chunks; // Number of chunks that were sent to this node, but didn't receive a receipt within the time allotted.
+	private int received_chunks; // Number of chunks received from this node.
+	private boolean cooperative; // This node can usually be trusted to adhere to good practices in the network.
+	private boolean blacklisted; // This node shouldn't be used, unless as a last resort.
+	private Set<RemoteNode> neighbors; // This node's neighbors
 }
