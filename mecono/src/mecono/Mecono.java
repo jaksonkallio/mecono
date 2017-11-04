12e00e747dca6b78f6dfea1dd7dd651baab8bcc8
Jakson Kallio <jakson@protonmail.com>
2017-11-04 18:12:35 -0500

    Software versioning

--- mecono/src/mecono/Mecono.java
+++ mecono/src/mecono/Mecono.java
@@ -22,6 +22,7 @@ public class Mecono extends Application {
 		}else{
 			if(simulated_network){
 				System.out.println("Running simulation");
+				stage.setTitle("Simulated Network - Mecono "+Mecono.getVersion());
 				sim = new SimNetwork();
 				sim.begin();
 				sim_gui = new SimGUI(sim);
@@ -40,6 +41,10 @@ public class Mecono extends Application {
     public static void main(String[] args) {	
 		launch();
     }
+	
+	public static String getVersion(){
+		return "v"+protocol_version+"."+version;
+	}
 
 	private void sandbox(){
 		
@@ -49,4 +54,6 @@ public class Mecono extends Application {
 	public static SimGUI sim_gui;
 	public static boolean simulated_network = true;
 	public static boolean sandbox = false;
+	public static int version = 1;
+	public static int protocol_version = 1;
 }
