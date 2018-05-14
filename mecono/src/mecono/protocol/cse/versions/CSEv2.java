2ccdb2ed9d2fb8adc9faea962f5b864f56f828ac
Jakson Kallio <jakson@protonmail.com>
2018-05-13 21:43:48 -0500

    Fixed neighborship creation

--- mecono/src/mecono/protocol/cse/versions/CSEv2.java
+++ mecono/src/mecono/protocol/cse/versions/CSEv2.java
@@ -18,7 +18,7 @@ public class CSEv2 extends SimNetwork {
 	
 	private void createNodes(){
 		for(int i = 0; i < node_count; i++){
-			node_set.add(new SimSelfNode("node"+i, new NodeAddress("n"+i), this));
+			node_set.add(new SimSelfNode("n"+i, new NodeAddress("n"+i), this));
 		}
 	}
 	
@@ -29,10 +29,19 @@ public class CSEv2 extends SimNetwork {
 	}
 	
 	private void createNeighborships(){
+		int[][] neighborships = {
+			{0,3,1,4,3,1,3,4,1,3,0,3,0,3,3,2,3,1,3,2,1,4,0,3,3,2,0,1,1,3,2,1,0,2,1,2,4,1,4,2,4,1,3,3,0,0,4,4,4,4},
+			{3,0,0,0,0,4,0,3,0,1,1,1,0,4,1,1,0,2,0,3,2,0,3,0,4,0,1,3,0,4,4,4,4,1,3,0,0,0,0,3,0,3,0,2,3,1,3,0,3,1},
+			{0,4,2,0,0,0,2,4,0,2,3,0,3,0,0,3,1,0,0,4,0,3,1,0,0,3,0,0,0,1,1,3,3,0,2,0,0,0,0,0,2,4,0,0,4,4,4,1,0,3},
+			{1,0,0,1,1,0,0,0,3,0,0,0,0,0,0,0,4,0,2,0,0,1,2,4,2,0,3,0,4,0,0,2,0,2,0,4,0,0,1,0,0,0,0,0,1,0,0,2,0,2}
+		};
+		
 		for(int i = 0; i < neighborships.length; i++){
 			for(int j = 0; j < neighborships[i].length; j++){
-				int neighbor_id = (j + neighborships[i][j]) % neighborships[i].length;
-				createNeighborship(node_set.get(j), node_set.get(neighbor_id));
+				if(neighborships[i][j] != 0){
+					int neighbor_id = (j + neighborships[i][j]) % neighborships[i].length;
+					createNeighborship(node_set.get(j), node_set.get(neighbor_id));
+				}
 			}
 		}
 	}
@@ -41,12 +50,6 @@ public class CSEv2 extends SimNetwork {
 	
 	// https://www.random.org/integers/?num=50&min=0&max=4&col=1&base=10&format=plain&rnd=new
 	// https://delim.co/
-	private final int[][] neighborships = {
-		{0,3,1,4,3,1,3,4,1,3,0,3,0,3,3,2,3,1,3,2,1,4,0,3,3,2,0,1,1,3,2,1,0,2,1,2,4,1,4,2,4,1,3,3,0,0,4,4,4,4},
-		{3,0,0,0,3,4,0,3,0,1,1,1,0,4,1,1,3,2,0,3,2,0,3,0,4,2,1,3,1,4,4,4,4,1,3,2,4,0,0,3,4,3,3,2,3,1,3,0,3,1},
-		{3,4,2,4,0,1,2,4,1,2,3,3,3,0,0,3,1,2,0,4,0,3,1,3,4,3,0,0,1,1,1,3,3,0,2,2,0,0,0,0,2,4,3,0,4,4,4,1,0,3},
-		{1,3,1,1,1,0,3,3,3,0,3,0,0,4,0,0,4,2,2,4,0,1,2,4,2,0,3,3,4,4,1,2,0,2,3,4,4,1,1,2,0,1,0,3,1,4,3,2,0,2}
-	};
 	
 	// https://www.random.org/integer-sets/?sets=50&num=2&min=0&max=99&commas=on&order=index&format=html&rnd=new
 	// http://textmechanic.com/text-tools/basic-text-tools/find-and-replace-text/
