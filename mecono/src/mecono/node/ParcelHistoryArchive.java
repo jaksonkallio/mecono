715d8cc866f88eba431ce2bf8fe889f106196458
Jakson Kallio <jakson@protonmail.com>
2018-05-17 16:37:06 -0700

    Parcel history archive class

---
+++ mecono/src/mecono/node/ParcelHistoryArchive.java
@@ -0,0 +1,10 @@
+package mecono.node;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import mecono.parceling.ParcelType;
+
+
+// Contains metadata about the parcels that were successfully sent into the network.
+public class ParcelHistoryArchive {
+}
