700206530d7aef2b4d1281976cd74a38099d0f4b
Jakson Kallio <jakson@protonmail.com>
2018-05-17 16:37:42 -0700

    Add new history item

--- mecono/src/mecono/node/ParcelHistoryArchive.java
+++ mecono/src/mecono/node/ParcelHistoryArchive.java
@@ -7,6 +7,17 @@ import mecono.parceling.ParcelType;
 
 // Contains metadata about the parcels that were successfully sent into the network.
 public class ParcelHistoryArchive {
+	
+	public void addParcelHistoryItem(String parcel_id, ParcelType parcel_type){
+		parcel_history.offer(new ParcelHistoryItem(parcel_id, parcel_type));
+	}
+	
+	// Number of previous parcels to keep
+	public final int HISTORY_LIMIT = 1000;
+	
+	// Parcel history item container
+	Queue<ParcelHistoryItem> parcel_history = new LinkedBlockingQueue<>();
+	
 	// Each parcel history item, has meta data.
 	private class ParcelHistoryItem {
 		public ParcelHistoryItem(String parcel_id, ParcelType parcel_type){
