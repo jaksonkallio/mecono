fddb2e254fda1ad375d1729b282ef954187baaa1
Jakson Kallio <jakson@protonmail.com>
2018-05-17 16:37:26 -0700

    History item inner class

--- mecono/src/mecono/node/ParcelHistoryArchive.java
+++ mecono/src/mecono/node/ParcelHistoryArchive.java
@@ -7,4 +7,15 @@ import mecono.parceling.ParcelType;
 
 // Contains metadata about the parcels that were successfully sent into the network.
 public class ParcelHistoryArchive {
+	// Each parcel history item, has meta data.
+	private class ParcelHistoryItem {
+		public ParcelHistoryItem(String parcel_id, ParcelType parcel_type){
+			this.parcel_id = parcel_id;
+			this.parcel_type = parcel_type;
+		}
+	
+		public final String parcel_id;
+		public boolean has_response = false;
+		public final ParcelType parcel_type;
+	}
 }
