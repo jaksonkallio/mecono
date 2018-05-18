a364314771c8b99de5646725f538a1b14f04dc61
Jakson Kallio <jakson@protonmail.com>
2018-05-18 13:15:41 -0700

    Use a more standard form of parcel creation. (even nodes send to odd nodes, plus offset of 7)

--- mecono/src/mecono/protocol/cse/versions/CSEv2.java
+++ mecono/src/mecono/protocol/cse/versions/CSEv2.java
@@ -25,61 +25,14 @@ public class CSEv2 extends SimNetwork {
 	}
 	
 	private void createParcels(){
-		int[][] parcel_set_raw = {
-			{11, 34},
-			{90, 41},
-			{73, 60},
-			{23, 10},
-			{45, 9},
-			{54, 80},
-			{13, 57},
-			{95, 28},
-			{80, 55},
-			{56, 3},
-			{10, 98},
-			{19, 45},
-			{60, 90},
-			{84, 63},
-			{77, 71},
-			{25, 95},
-			{40, 73},
-			{87, 80},
-			{56, 77},
-			{48, 6},
-			{29, 4},
-			{98, 67},
-			{57, 64},
-			{35, 0},
-			{26, 43},
-			{11, 10},
-			{69, 48},
-			{87, 20},
-			{73, 59},
-			{43, 76},
-			{61, 99},
-			{66, 88},
-			{13, 51},
-			{61, 93},
-			{73, 31},
-			{11, 26},
-			{87, 2},
-			{47, 17},
-			{69, 76},
-			{88, 11},
-			{95, 3},
-			{3, 13},
-			{4, 15},
-			{73, 96},
-			{73, 45},
-			{4, 45},
-			{85, 16},
-			{28, 95},
-			{36, 21},
-			{3, 31}
-		};
+		int standard_dest_offset = 7;
 		
-		for(int i = 0; i < parcel_set_raw.length; i++){
-			addSampleParcel(parcel_set_raw[i][0],parcel_set_raw[i][1]);
+		if(standard_dest_offset % 2 == 1){
+			for(int i = 0; i < PARCEL_COUNT; i++){
+				if(i % 2 == 0){
+					addSampleParcel(i, ((i + standard_dest_offset) % node_count));
+				}
+			}
 		}
 	}
 	
@@ -108,4 +61,6 @@ public class CSEv2 extends SimNetwork {
 	
 	// https://www.random.org/integer-sets/?sets=50&num=2&min=0&max=99&commas=on&order=index&format=html&rnd=new
 	// http://textmechanic.com/text-tools/basic-text-tools/find-and-replace-text/
+	
+	public final int PARCEL_COUNT = 50;
 }
