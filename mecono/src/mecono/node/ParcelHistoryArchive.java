0f1d6410589b23684749cf782210e064edca940a
Jakson Kallio <jakson@protonmail.com>
2018-07-04 12:06:30 -0700

    Time included in parcel history archive

--- mecono/src/mecono/node/ParcelHistoryArchive.java
+++ mecono/src/mecono/node/ParcelHistoryArchive.java
@@ -2,14 +2,15 @@ package mecono.node;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import mecono.parceling.DestinationParcel;
 import mecono.parceling.ParcelType;
 
 
 // Contains metadata about the parcels that were successfully sent into the network.
 public class ParcelHistoryArchive {
 	
-	public void addParcelHistoryItem(String parcel_id, ParcelType parcel_type){
-		parcel_history.offer(new ParcelHistoryItem(parcel_id, parcel_type));
+	public void addParcelHistoryItem(DestinationParcel parcel){
+		parcel_history.offer(new ParcelHistoryItem(parcel.getUniqueID(), parcel.getParcelType(), parcel.getTimeSent()));
 		trimOld();
 	}
 	
@@ -48,13 +49,15 @@ public class ParcelHistoryArchive {
 	
 	// Each parcel history item, has meta data.
 	private class ParcelHistoryItem {
-		public ParcelHistoryItem(String parcel_id, ParcelType parcel_type){
+		public ParcelHistoryItem(String parcel_id, ParcelType parcel_type, long time_sent){
 			this.parcel_id = parcel_id;
 			this.parcel_type = parcel_type;
+			this.time_sent = time_sent;
 		}
 	
 		public final String parcel_id;
 		public boolean has_response = false;
+		public long time_sent = 0; // Epoch millis
 		public final ParcelType parcel_type;
 	}
 }
