d765ee505bfd2a14e4a428b4c066d40a1ccf1135
Jakson Kallio <jakson@protonmail.com>
2018-07-26 19:24:36 -0700

    HandshakeHistory refactor

--- mecono/src/mecono/node/HandshakeHistory.java
+++ mecono/src/mecono/node/HandshakeHistory.java
@@ -4,18 +4,17 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import mecono.parceling.BadPathException;
 import mecono.parceling.DestinationParcel;
 import mecono.parceling.MissingParcelDetailsException;
 import mecono.parceling.Handshake;
-import mecono.protocol.BadProtocolException;
+import mecono.parceling.types.PingParcel;
 
 /**
  * The Sent Parcel History is a complete archive of recently sent, or queued to be sent, parcels into the Mecono network.
  * @author Jakson
  */
-public class SentParcelHistory {
-	public SentParcelHistory(Mailbox mailbox){
+public class HandshakeHistory {
+	public HandshakeHistory(Mailbox mailbox){
 		this.mailbox = mailbox;
 	}
 	
@@ -36,28 +35,30 @@ public class SentParcelHistory {
 	
 	public void attemptSend(){
 		if(send_cursor < pending.size()){
-			Handshake send = pending.get(send_cursor);
+			Handshake handshake = pending.get(send_cursor);
 			
 			// First check consists of readiness based on outbound information
 			// Must be unsent, must not be stale, and must be ready to resend
-			if(!send.isSent()
-					&& !send.isStale()
-					&& send.readyResend()){
-				DestinationParcel original_parcel = send.getOriginalParcel();
+			if(!handshake.isSent()
+					&& !handshake.isStale()
+					&& handshake.readyResend()){
+				DestinationParcel original_parcel = handshake.getOriginalParcel();
 				
 				try {
 					// Second check consists of readiness based on the actual parcel metadata
 					if(original_parcel.pathKnown()){
 						if(original_parcel.pathOnline()){
-							
+							// Send
 						}else{
-							pingPath(original_parcel.getOutboundActualPath());
+							// Ping the destination
+							pingPath(original_parcel.getActualPath());
 						}
 					}else{
+						// Consult for a path to the destination
 						consultPath(((RemoteNode) original_parcel.getDestination()));
 					}
 				} catch(MissingParcelDetailsException ex){
-				
+					
 				}
 			}
 		}
@@ -66,12 +67,27 @@ public class SentParcelHistory {
 		send_cursor = (send_cursor + 1) % pending.size();
 	}
 	
-	private void pingPath(PathStats path){
-	
+	private void pingPath(Path path){
+		PingParcel ping = new PingParcel(mailbox, DestinationParcel.TransferDirection.OUTBOUND);
+		ping.setDestination((RemoteNode) path.getLastStop());
+		
+		if(!alreadyPending(ping)){
+			enqueueSend(ping);
+		}
 	}
 	
 	private void consultPath(RemoteNode target){
+		
+	}
 	
+	private boolean alreadyPending(DestinationParcel parcel){
+		for(Handshake handshake : pending){
+			if(handshake.getOriginalParcel().equals(parcel)){
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	private int send_cursor = 0;
