189d0532ea18e56f419f1e46b6052f2087a42905
Jakson Kallio <jakson@protonmail.com>
2018-08-04 10:54:43 -0700

    Map edges

--- mecono/src/mecono/ui/VisualNetworkMap.java
+++ mecono/src/mecono/ui/VisualNetworkMap.java
@@ -13,6 +13,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import mecono.node.Neighbor;
+import mecono.node.Node;
+import mecono.node.RemoteNode;
 import mecono.node.SimSelfNode;
 import mecono.protocol.cse.SimNetwork;
 
@@ -46,7 +49,21 @@ public class VisualNetworkMap extends Stage {
 			map_canvas.getChildren().add(map_node.getVisualNode());
 		}
 		
+		for(MapEdge map_edge : map_edges){
+			map_canvas.getChildren().add(map_edge.getEdge());
+		}
+		
 		return map_canvas;
+	}
+	
+	private MapNode lookupMapNode(Node node){
+		for(MapNode map_node : map_nodes){
+			if(map_node.getSimSelfNode().equals(node)){
+				return map_node;
+			}
+		}
+		
+		return null;
 	}
 	
 	private class MapNode {
@@ -54,6 +71,7 @@ public class VisualNetworkMap extends Stage {
 			this.node = node;
 		}
 		
+		@Override
 		public boolean equals(Object o){
 			if(o instanceof MapNode){
 				MapNode other = (MapNode) o;
@@ -75,9 +93,17 @@ public class VisualNetworkMap extends Stage {
 			return list_index % cells_row;
 		}
 		
+		public int getX(){
+			return (getCellX() + 1) * node_hor_spacing;
+		}
+		
 		public int getCellY(){
 			int list_index = map_nodes.indexOf(this);
 			return (int) (list_index / cells_row);
+		}
+		
+		public int getY(){
+			return (getCellY() + 1) * node_ver_spacing;
 		}
 		
 		public Pane getVisualNode(){
@@ -86,9 +112,18 @@ public class VisualNetworkMap extends Stage {
 			Label vis_node_label = new Label(node.getAddress());
 			vis_node_label.setFont(UtilGUI.MICRO_LABEL);
 			vis_node_circle.setRadius(node_radius);
-			stack.relocate((getCellX() + 1) * node_spacing, (getCellY() + 1) * node_spacing);
+			stack.relocate(getX(), getY());
 			vis_node_circle.setStroke(Color.BLACK);
 			vis_node_circle.setFill(Color.WHITE);
+			
+			for(Neighbor neighbor : node.getNeighbors()){
+				Node neighbor_node = neighbor.getNode();
+				MapEdge map_edge = new MapEdge(this, lookupMapNode(neighbor_node));
+				
+				if(!map_edges.contains(map_edge)){
+					map_edges.add(map_edge);
+				}
+			}
 			
 			stack.getChildren().addAll(vis_node_circle, vis_node_label);
 			
@@ -98,9 +133,55 @@ public class VisualNetworkMap extends Stage {
 		private final SimSelfNode node;
 	}
 	
+	private class MapEdge {
+		public MapEdge(MapNode node1, MapNode node2){
+			this.node1 = node1;
+			this.node2 = node2;
+		}
+		
+		@Override
+		public boolean equals(Object o){
+			if(o instanceof MapEdge){
+				MapEdge other = (MapEdge) o;
+				
+				if(other.getNode(1).equals(this.getNode(1)) && other.getNode(2).equals(this.getNode(2))){
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		
+		public Line getEdge(){
+			Line vis_edge = new Line();
+			
+			vis_edge.setStartX(node1.getX());
+			vis_edge.setStartY(node1.getY());
+			vis_edge.setEndX(node2.getX());
+			vis_edge.setEndY(node2.getY());
+			
+			return vis_edge;
+		}
+		
+		public MapNode getNode(int i){
+			if(i == 1){
+				return node1;
+			}
+			
+			return node2;
+		}
+		
+		private final MapNode node1;
+		private final MapNode node2;
+	}
+	
 	private final ArrayList<MapNode> map_nodes = new ArrayList<>();
+	private final ArrayList<MapEdge> map_edges = new ArrayList<>();
 	private final SimNetwork sim;
 	private final int cells_row = 8;
-	private final int node_spacing = 40;
+	private final int win_width = 800;
+	private final int win_height = 800;
+	private final int node_hor_spacing = win_width / (cells_row + 1);
+	private final int node_ver_spacing = 40;
 	private final int node_radius = 10;
 }
