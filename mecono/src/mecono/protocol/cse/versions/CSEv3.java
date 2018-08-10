bd156c05ecb0ca20dc1e3bf0611b7d69a9b17097
Jakson Kallio <jakson@protonmail.com>
2018-08-10 14:03:49 -0700

    Create CSEv3 class

---
+++ mecono/src/mecono/protocol/cse/versions/CSEv3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mecono.protocol.cse.versions;
+
+import mecono.protocol.cse.SimNetwork;
+
+/**
+ *
+ * @author Jakson
+ */
+public class CSEv3 extends SimNetwork {
+
+	@Override
+	protected void initEnvironment() {
+		
+	}
+	
+}
