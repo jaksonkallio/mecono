d6a141b53081d3ad1733079db5a6739fb6ef59ee
Jakson Kallio <jakson@protonmail.com>
2018-08-31 10:04:54 -0500

    Announce Parcel object

---
+++ mecono/src/mecono/parceling/types/AnnounceParcel.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mecono.parceling.types;
+
+import mecono.node.Mailbox;
+import mecono.node.Path;
+import mecono.parceling.DestinationParcel;
+import mecono.parceling.MissingParcelDetailsException;
+import mecono.protocol.BadProtocolException;
+
+/**
+ *
+ * @author sabreok
+ */
+public class AnnounceParcel extends DestinationParcel {
+	
+	public AnnounceParcel(Mailbox mailbox, TransferDirection direction) {
+		super(mailbox, direction);
+	}
+	
+	@Override
+	public void onReceiveAction() throws BadProtocolException, MissingParcelDetailsException {
+		super.onReceiveAction();
+		
+	}
+	
+	public void setAnnounceChain(Path announce_chain){
+		this.announce_chain = announce_chain;
+	}
+	
+	private Path announce_chain;
+}
