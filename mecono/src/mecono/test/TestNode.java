ba074684eb9e17ddf772ac0b98442caa25bc3d96
Jakson Kallio <jakson@protonmail.com>
2018-11-19 11:09:42 -0600

    TestNode test_self object

--- mecono/src/mecono/test/TestNode.java
+++ mecono/src/mecono/test/TestNode.java
@@ -1,8 +1,16 @@
 package mecono.test;
 
+import mecono.node.SelfNode;
+
 public class TestNode extends Test {
+	public TestNode(){
+		test_self = new SelfNode();
+	}
+	
 	@Override
 	public void test(){
 		
 	}
+	
+	private final SelfNode test_self;
 }
