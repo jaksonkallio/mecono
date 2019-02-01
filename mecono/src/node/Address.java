bf68a8bb8515e6e00ee3f8f93427efc383ec032a
Jakson Kallio <jakson@protonmail.com>
2019-02-01 10:59:58 -0600

    Class members

---
+++ mecono/src/node/Address.java
@@ -0,0 +1,17 @@
+package node;
+
+public class Address {
+	public Address(String address){
+		this.address = address;
+	}
+	
+	public static Address generate(){
+		
+	}
+	
+	public String getString(){
+		return address;
+	}
+	
+	private final String address;
+}
--- mecono/src/node/Node.java
+++ mecono/src/node/Node.java
@@ -1,5 +1,27 @@
 package node;
 
 public class Node {
-
+	public Node(Address address){
+		this.address = address;
+	}
+	
+	public static Node generate(){
+		
+	}
+	
+	public String getAddressString(){
+		return address.getString() + '!' + coords.x + ',' + coords.y;
+	}
+	
+	public void setLabel(String label){
+		this.label = label;
+	}
+	
+	public void setCoords(GeoCoord coords){
+		this.coords = coords;
+	}
+	
+	private String label;
+	private final Address address;
+	private GeoCoord coords;
 }
