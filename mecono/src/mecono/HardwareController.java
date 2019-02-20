fea0d226e4580081d58d8243b4a7c0ce41db2eac
Jakson Kallio <jakson@protonmail.com>
2019-02-20 10:47:00 -0600

    Hardware controller start

---
+++ mecono/src/mecono/HardwareController.java
@@ -0,0 +1,40 @@
+package mecono;
+
+import java.util.ArrayList;
+import java.util.List;
+import node.Connection;
+
+public class HardwareController {
+	public HardwareController(){
+		port_connections = new ArrayList<>();
+	}
+	
+	public void addConnection(int port, Connection connection){
+		PortConnection c = new PortConnection();
+		c.port = port;
+		c.connection = connection;
+		
+		if(!port_connections.contains(c)){
+			port_connections.add(c);
+		}
+	}
+	
+	private class PortConnection {
+		public boolean equals(Object o){
+			if(o instanceof PortConnection){
+				PortConnection other = (PortConnection) o;
+				
+				if(this.port == other.port && this.connection.equals(other.connection)){
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		
+		public int port;
+		public Connection connection;
+	}
+
+	private final List<PortConnection> port_connections;
+}
---
+++ mecono/src/mecono/SimHardwareController.java
@@ -0,0 +1,5 @@
+package mecono;
+
+public class SimHardwareController extends HardwareController {
+	
+}
